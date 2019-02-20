c3feb74267b57371333e291869809b9c4527155e
unknown <duncangichimu@gmail.com>
2019-02-19 18:48:06 -0800

    (fix) nginx deployment config

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -a
 source .env
-envsubst '$$NGINX_HOST' < ./nginx/template.conf > ./nginx/nginx.conf
+envsubst '$$NGINX_HOST' < ./nginx/template.conf > ./nginx/default.conf
 cat docker-compose.yml | envsubst | docker-compose -f - build

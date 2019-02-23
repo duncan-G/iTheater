c281a8cc1347f6bf590ad8ad7e4da736f1c16273
unknown <duncangichimu@gmail.com>
2019-02-22 18:09:42 -0800

    test

--- client/src/app/core/http.inteceptors.ts
+++ client/src/app/core/http.inteceptors.ts
@@ -1,16 +1,1 @@
-// Test
-// Test2
-// Test3
-// Test 4
-// Test 5
-// Test 6
-// Test 7
-// Tesat 8
-// Test 9
-// Test 10
-// test 11
-// Test 12
-// Test
-// Another one
-// Am I done?
-// Test last?
+

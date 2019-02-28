6752de804a27c83ba7742a7ec6d49214f654f54b
unknown <duncangichimu@gmail.com>
2019-02-27 19:58:00 -0800

    add landing page module

---
+++ client/src/app/main/landing/landing.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { popularMovies } from "../../fake-db/popular-movies";
+
+@Component({
+  selector: "app-landing",
+  templateUrl: "./landing.component.html",
+  styleUrls: ["./landing.component.scss"],
+  encapsulation: ViewEncapsulation.None
+})
+
+export class LandingComponent implements OnInit {
+  public popularMovies: any;
+  public imageBaseUrl = "http://image.tmdb.org/t/p/original/";
+
+  constructor() {
+    this.popularMovies = popularMovies.results;
+    console.log(this.popularMovies);
+  }
+
+  ngOnInit() {}
+
+  formatBgImg(imgUrl) {
+    return `url("${this.imageBaseUrl + imgUrl}"`;
+  }
+}
---
+++ client/src/app/main/landing/landing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { LandingComponent } from "./landing.component";
+import { RouterModule } from "@angular/router";
+import { SharedModule } from "src/app/shared/shared.modules";
+import { MatButtonModule, MatIconModule } from "@angular/material";
+
+const routes = [
+  {
+    path: "",
+    component: LandingComponent
+  }
+];
+@NgModule({
+  declarations: [LandingComponent],
+  imports: [RouterModule.forChild(routes), MatButtonModule, MatIconModule, SharedModule]
+})
+export class LandingModule {}

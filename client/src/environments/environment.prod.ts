13c235d41f5c2b288a42df8dc4a69963f60086ab
unknown <duncangichimu@gmail.com>
2019-03-01 16:28:35 -0800

    add new env variables

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
-  hmr: false
+  hmr: false,
+  defaultPosterUrl: '/assets/movie-poster-default.png',
+  posterBaseUrl: "http://image.tmdb.org/t/p/w300/",
+  posterSmallBaseUrl: 'http://image.tmdb.org/t/p/w92/',
+  backdropBaseUrl: "http://image.tmdb.org/t/p/original/",
+  serverUrl: "https://ec2:5001/",
+  movieBaseUrl: 'https://api.themoviedb.org/3/',
+  moviePublicKey: '4828e7339a2fec105560e72dd6d5dd36',
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,14 @@
 
 export const environment = {
   production: false,
-  hmr: true
+  hmr: true,
+  defaultPosterUrl: '/assets/movie-poster-default.png',
+  posterBaseUrl: 'http://image.tmdb.org/t/p/w300/',
+  posterSmallBaseUrl: 'http://image.tmdb.org/t/p/w92/',
+  backdropBaseUrl: 'http://image.tmdb.org/t/p/original/',
+  serverUrl: 'https://localhost:5001/',
+  movieBaseUrl: 'https://api.themoviedb.org/3/',
+  moviePublicKey: '4828e7339a2fec105560e72dd6d5dd36',
 };
 
 /*

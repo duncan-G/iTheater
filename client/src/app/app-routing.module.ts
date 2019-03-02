77ad90e4578655777d51fa2b3293c9bd30bbd4ff
unknown <duncangichimu@gmail.com>
2019-03-01 16:27:41 -0800

    move lazy loading of each route into routing module. MainModule is no longer needed

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { ReverseAuthGuard } from "./core/guards/reverse.auth.guard";
+import { AuthGuard } from "./core/guards/auth.guard";
 
 const routes: Routes = [
   {
     path: "",
-    loadChildren: "./main/main.module#MainModule"
+    loadChildren: "./main/landing/landing.module#LandingModule"
+  },
+  {
+    path: "",
+    loadChildren: "./main/login/login.module#LoginModule",
+    canActivate: [ReverseAuthGuard]
+  },
+  {
+    path: "",
+    loadChildren: "./main/register/register.module#RegisterModule",
+    canActivate: [ReverseAuthGuard]
+  },
+  {
+    path: "",
+    loadChildren:
+      "./main/reset-password/reset-password.module#ResetPasswordModule",
+    canActivate: [ReverseAuthGuard]
+  },
+  {
+    path: "",
+    loadChildren: "./main/movie-lists/movie-lists.module#MovieListsModule",
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "",
+    loadChildren: "./main/movie-list/movie-list.module#MovieListModule"
+  },
+  {
+    path: "",
+    loadChildren: "./main/movie/movie.module#MovieModule"
   },
   { path: "**", redirectTo: "/error-404" }
 ];

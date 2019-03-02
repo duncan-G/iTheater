58caceac740bee32bbd370b80e92aeba9550efb8
unknown <duncangichimu@gmail.com>
2019-03-01 16:31:56 -0800

    import modules into app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule } from "@angular/common/http";
+import { EmbedVideo } from "ngx-embed-video";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.modules';
-import { NavbarModule } from './main/navbar/navbar.module';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from "./core/core.module";
+import { NavbarModule } from "./main/navbar/navbar.module";
+import { Page500Module } from "./main/page500/page500.module";
+import { Page404Module } from "./main/page404/page404.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    CoreModule,
+    HttpClientModule,
+    EmbedVideo.forRoot(),
+    Page404Module,
+    Page500Module,
     NavbarModule,
     SharedModule,
-    AppRoutingModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

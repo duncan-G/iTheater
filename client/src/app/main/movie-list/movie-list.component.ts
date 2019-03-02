7d57046b68a5abe6db9b3c9e93b3108501db540e
unknown <duncangichimu@gmail.com>
2019-03-01 16:29:26 -0800

    style movie list component and make buttons work

--- client/src/app/main/movie-list/movie-list.component.ts
+++ client/src/app/main/movie-list/movie-list.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { movieListsWithMovies } from "src/app/fake-db/movie-lists";
+import { MatDialog, MatDialogRef } from "@angular/material";
+
+import { AddNewMovieComponent } from "./add-new-movie/add-new-movie.component";
 
 @Component({
-  selector: 'app-movie-list',
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.scss']
+  selector: "app-movie-list",
+  templateUrl: "./movie-list.component.html",
+  styleUrls: ["./movie-list.component.scss"],
+  encapsulation: ViewEncapsulation.None
 })
 export class MovieListComponent implements OnInit {
+  private moviesList: any;
+  public movies: any[];
+  public listName: string;
+  private dialogRef: MatDialogRef<AddNewMovieComponent>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: ActivatedRoute, private dialog: MatDialog) {
+    this.moviesList = movieListsWithMovies;
   }
 
+  ngOnInit() {
+    this.router.params.subscribe(({ listName }) => {
+      this.listName = listName;
+      this.movies = this.moviesList[listName];
+    });
+  }
+
+  openDialog() {
+    this.dialogRef = this.dialog.open(AddNewMovieComponent, {
+      width: "70%",
+      maxWidth: "900px",
+      height: "60%",
+      maxHeight: "800px"
+    });
+  }
 }
--- client/src/app/main/movie-list/movie-list.module.ts
+++ client/src/app/main/movie-list/movie-list.module.ts
@@ -1,15 +1,41 @@
 import { NgModule } from "@angular/core";
 import { MovieListComponent } from "./movie-list.component";
 import { RouterModule } from "@angular/router";
+import { BarRatingModule } from "ngx-bar-rating";
+import { MovieItemComponent } from "./movie-item/movie-item.component";
+import { SharedModule } from "src/app/shared/shared.module";
+import {
+  MatTooltipModule,
+  MatButtonModule,
+  MatIconModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatFormFieldModule,
+  MatInputModule
+} from "@angular/material";
+import { AddNewMovieComponent } from "./add-new-movie/add-new-movie.component";
+import { MovieSearchResultsComponent } from './movie-search-results/movie-search-results.component';
 
 const routes = [
   {
-    path: "my-theater/:movie-list",
+    path: "my-theater/:listName",
     component: MovieListComponent
   }
 ];
 @NgModule({
-  declarations: [MovieListComponent],
-  imports: [RouterModule.forChild(routes)]
+  declarations: [MovieListComponent, MovieItemComponent, AddNewMovieComponent, MovieSearchResultsComponent],
+  imports: [
+    RouterModule.forChild(routes),
+    MatDialogModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule,
+    BarRatingModule,
+    SharedModule
+  ],
+  entryComponents: [AddNewMovieComponent]
 })
-export class MovieListModule {} 
+export class MovieListModule {}

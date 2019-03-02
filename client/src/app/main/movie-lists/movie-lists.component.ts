7389b2e0ce06eb7879c0f263ec736db8c241eda4
unknown <duncangichimu@gmail.com>
2019-03-01 16:23:28 -0800

    finish movie lists component

--- client/src/app/main/movie-lists/movie-lists.component.ts
+++ client/src/app/main/movie-lists/movie-lists.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { movieListWithImage } from "../../fake-db/movie-lists";
+import { MovieListsService } from "src/app/core/services/movie-lists.service";
+import { MovieList } from "src/app/core/models/movie-list.interface";
+import { MatDialog, MatDialogRef } from "@angular/material";
+
+import { AddNewListComponent } from "./add-new-list/add-new-list.component";
 
 @Component({
   selector: "app-movie-lists",
@@ -8,11 +13,27 @@ import { movieListWithImage } from "../../fake-db/movie-lists";
   encapsulation: ViewEncapsulation.None
 })
 export class MovieListsComponent implements OnInit {
-  public movieLists: any[];
+  public movieLists: MovieList[];
+  private dialogRef : MatDialogRef<AddNewListComponent>;
 
-  constructor() {
-    this.movieLists = movieListWithImage;
+  constructor(
+    private movieListService: MovieListsService,
+    private dialog: MatDialog
+  ) {}
+
+  ngOnInit() {
+    this.movieListService.fetchData().subscribe(() => {
+      this.movieListService.movieLists.subscribe(
+        data => (this.movieLists = data)
+      );
+    });
   }
 
-  ngOnInit() {}
+  openDialog() {
+    this.dialogRef = this.dialog.open(AddNewListComponent);
+  }
+
+  ngOnDestroy(){
+    this.dialogRef && this.dialogRef.close();
+  }
 }
